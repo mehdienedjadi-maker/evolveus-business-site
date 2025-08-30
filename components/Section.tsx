20047e1ec672942aa52567dd035eddc8e47b8849
mehdienedjadi-maker <mehdie.nedjadi@gmail.com>
2025-08-30 14:48:08 +0200

    Create Section.tsx

---
+++ components/Section.tsx
@@ -0,0 +1,10 @@
+export default function Section() {
+  return (
+    <section className="p-6 border rounded-xl">
+      <h3 className="text-xl font-medium">Découvrir les solutions</h3>
+      <p className="mt-2 text-gray-600">
+        Technologie qu’on aime. Bien-être au travail.
+      </p>
+    </section>
+  );
+}
