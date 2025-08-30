43f8d08ad49208ea196186fd0005fb81105c00c7
mehdienedjadi-maker <mehdie.nedjadi@gmail.com>
2025-08-30 11:57:36 +0200

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import "./globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Evolve'Us Business",
+  description: "Suite logicielle & lifestyle pour le bien-être en entreprise",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="fr">
+      <body className="text-deep antialiased">{children}</body>
+    </html>
+  );
+}
